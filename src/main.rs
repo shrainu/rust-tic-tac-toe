785df639804b5b2ec36c0acd0281ea23a436a106
Batuhan Yigit <batuhanyigit1705@gmail.com>
2022-03-08 01:51:14 +0300

    actions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::{stdin, stdout, Write};
+use crate::Action::{Place, Quit, Unknown};
 
 fn get_line(msg : &str) -> io::Result<String> {
 
@@ -15,35 +16,79 @@ fn get_line(msg : &str) -> io::Result<String> {
         return Err(e);
     }
 
+    line.remove(line.len() - 1);
+
     return Ok(line);
 }
 
-fn get_number(msg : &str) -> Result<i32, String> {
+fn parse_number(line : &String) -> Result<i32, String> {
 
-    let line = get_line(msg);
+    return match line.parse::<i32>() {
+        Ok(i) => {
+            Ok(i)
+        }
+        Err(_) => {
+            let err = format!("ERROR: Could not parse i32 from `{}`", line);
+            Err(err)
+        }
+    }
+}
 
-    return match line {
-        Ok(mut l) => {
+static TABLE: [i32;9] = [0; 9];
+static mut RUNNING : bool = false;
 
-            l.remove(l.len() - 1);
+fn get_running() -> bool {
+    unsafe {
+        return RUNNING;
+    }
+}
 
-            if let Ok(i) = l.parse::<i32>() {
-                Ok(i)
-            } else {
-                let err = format!("ERROR: Could not parse i32 from `{}`", l);
-                Err(err)
-            }
-        },
-        Err(e) => Err(e.to_string())
+fn set_running(running : bool) {
+    unsafe {
+        RUNNING = running;
+    }
+}
+
+enum Action {
+    Unknown,
+    Quit,
+    Place(i32),
+}
+
+fn get_action() -> Action {
+
+    let input = get_line(">> ");
+
+    let line = match input {
+        Ok(mut l) => l,
+        _ => String::new()
+    };
+
+    if line.to_lowercase() == "quit" {
+        return Quit;
+    } else if let Ok(i) = parse_number(&line) {
+        return Place(i);
+    } else {
+        return Unknown;
     }
 }
 
 fn main() {
 
-    let num = get_number("Enter a number: ");
+    set_running(true);
 
-    match num {
-        Ok(i) => { println!("{}^2 = {}", i, i * i); },
-        Err(e) => { println!("{}", e); }
+    while get_running() {
+
+        match get_action() {
+            Quit => {
+                set_running(false);
+            }
+            Place(i) => {
+                println!("Place : {}", i);
+            }
+            Unknown => {
+                println!("Unknown action.");
+            }
+        }
     }
 }
